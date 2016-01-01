384b40683990568911c6c3f44338f3d69bc37f62
Rishabh Gupta <r.g.gupta@outlook.com>
2016-01-01 18:22:11 +0530

    Implements get_playlist

--- PlayExt.py
+++ PlayExt.py
@@ -10,7 +10,22 @@ import sys
 import os
 
 def get_playlist(url):
-    pass
+
+    source_code = urllib.urlopen(url)
+    soup = BS(source_code.read(), "lxml")
+
+    href = []      # contains all links
+    title = []     # contains all titles
+    dicti = {}     # contains title and corresponding link
+
+    for link in soup.findAll('a', {'class':'pl-video-title-link'}):
+        href.append("https://www.youtube.com" + link.get('href'))  # Get link
+        title.append(link.string)                                  # Get title
+        title[-1] = title[-1][7:-5]
+        dicti[title[-1]] = href[-1]
+
+    for x in href:
+        get_download(x)        # Send to download
 
 
 def get_download(link):
